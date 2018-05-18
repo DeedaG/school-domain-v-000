8a72b2a5657808b15159b5023255f1da46886603
Christia T <christiatidwell@bellsouth.net>
2018-05-18 19:21:04 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,20 @@
 # code here!
+class School
+
+  attr_reader :roster
+
+  def initialize(roster)
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    @roster = { grade => [student_name]}
+    if @roster[grade][student_name].includes?(grade)
+      @roster[grade] = []
+    else @roster[grade]<< student_name
+    end
+      
+  end
+
+
+end
